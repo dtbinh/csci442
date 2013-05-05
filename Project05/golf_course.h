e0b32be4751c4a1666851271a556a814250c98f0
Matt Buland <gik0geck0@gmail.com>
2013-05-05 14:28:21 -0600

    trying out some locks

--- Project05/golf_course.h
+++ Project05/golf_course.h
@@ -3,34 +3,46 @@
 #include <vector>
 
 struct GolfCourse {
+    vector<boost::barrier*> hole_barriers;
+    vector<boost::barrier*> party_barriers;
+    vector<boost::mutex*> hole_locks;
+    vector<boost::condition_variable*> hole_conditions;
 
-  GolfCourse() {
-    for (int i = 0; i < 18; i++) {
-      // This is where you will initialize your barriers, condition variables,
-      // and any other shared variables or synchronization primitives.
-    }
-  }
+    vector<int*> party_nums;
+    vector<int*> party_playing_hole;
 
-  // Compute the average turnaround time over all the golfers that have
-  // completed the course.
-  double average_turnaround_time() {
-    boost::xtime sum;
-    sum.sec = 0;
-    sum.nsec = 0;
+	GolfCourse() {
+		for (int i = 0; i < 18; i++) {
+			// This is where you will initialize your barriers, condition variables,
+			// and any other shared variables or synchronization primitives.
+            hole_barriers.push_back(new boost::barrier(4));
+            party_barriers.push_back(new boost::barrier(4));
+            party_playing_hole.push_back(new int(-1));
+            hole_locks.push_back(new boost::mutex());
+            hole_conditions.push_back(new condition_variable);
+		}
+	}
 
-    vector<boost::xtime>::iterator it;
+	// Compute the average turnaround time over all the golfers that have
+	// completed the course.
+	double average_turnaround_time() {
+		boost::xtime sum;
+		sum.sec = 0;
+		sum.nsec = 0;
 
-    for (it = turnaround_times.begin(); it != turnaround_times.end(); ++it) {
-      sum.sec += it->sec;
-      sum.nsec += it->nsec;
-    }
+		vector<boost::xtime>::iterator it;
 
-    double sum_sec = (double) sum.sec + ((double) sum.nsec) / 1000000000.0;
-    return sum_sec / turnaround_times.size();
-  }
+		for (it = turnaround_times.begin(); it != turnaround_times.end(); ++it) {
+			sum.sec += it->sec;
+			sum.nsec += it->nsec;
+		}
 
-  // This is where you will define your barriers, condition variables, and any
-  // other shared variables or synchronization primitives. The variables are
-  // currently public to keep it simple.
-  vector<boost::xtime> turnaround_times;
+		double sum_sec = (double) sum.sec + ((double) sum.nsec) / 1000000000.0;
+		return sum_sec / turnaround_times.size();
+	}
+
+	// This is where you will define your barriers, condition variables, and any
+	// other shared variables or synchronization primitives. The variables are
+	// currently public to keep it simple.
+	vector<boost::xtime> turnaround_times;
 };
--- Project05/player.h
+++ Project05/player.h
@@ -9,110 +9,144 @@
 
 class Player {
 public:
-  Player(int id, string name, double m, double s, int pNum,
-      GolfCourse* course, boost::xtime start) :
-    myId(id), myName(name), mean(m), sigma(s), myParty(pNum), gc(course),
-    startTime(start) {
+	Player(int id, string name, double m, double s, int pNum,
+			GolfCourse* course, boost::xtime start) :
+		myId(id), myName(name), mean(m), sigma(s), myParty(pNum), gc(course),
+		startTime(start) {
 
-    // Seed our random number generator. Comment out these lines if you want to
-    // test your code on a constant play durations
-    unsigned int seed = static_cast<unsigned int>(std::time(0));
-    rng.seed(static_cast<unsigned int>(seed));
+		// Seed our random number generator. Comment out these lines if you want to
+		// test your code on a constant play durations
+		unsigned int seed = static_cast<unsigned int>(std::time(0));
+		rng.seed(static_cast<unsigned int>(seed));
 
-    // Set up the generator the will spit out numbers from the log normal
-    // distribution.  Once the generator is set up, you get random numbers
-    // from it by calling it like a function, that is, operator()
-    log_norm = new boost::lognormal_distribution<>(mean, sigma);
-    log_norm_generator = new boost::variate_generator<boost::mt19937,
-       boost::lognormal_distribution<> >(rng, *log_norm);
+		// Set up the generator the will spit out numbers from the log normal
+		// distribution.	Once the generator is set up, you get random numbers
+		// from it by calling it like a function, that is, operator()
+		log_norm = new boost::lognormal_distribution<>(mean, sigma);
+		log_norm_generator = new boost::variate_generator<boost::mt19937,
+			 boost::lognormal_distribution<> >(rng, *log_norm);
 
-    boost::mutex::scoped_lock io_lock(io_mutex);
+		boost::mutex::scoped_lock io_lock(io_mutex);
 
-    cout << "+++++ Player #" << myId << " (" << myName
-       << ") added to party " << myParty << endl;;
-  }
+        if (!int_in(*(gc->party_nums), &myParty) ) {
+            // create their party barrier
+            gc->party_nums->push_back(new int(myParty));
+            gc->party_barrier->push_back(new boost::barrier(4));
+        }
 
-  // This operator and the included function get called when the thread is
-  // created
-  void operator()() {
-    playCourse();
-  }
+		cout << "+++++ Player #" << myId << " (" << myName
+			 << ") added to party " << myParty << endl;
+	}
 
-  // I don't think you will need to modfiy this function.
-  void playCourse() {
-    // Playing the course simply means playing each hole.
-    for (int i = 0; i < 18; i++) {
-      playHole(i);
-    }
+	bool int_in(vector<int*> vect, int* item) {
+        for ( int i=0; i < vect->size(); i++) {
+            if ( *(vect[i]) == *item ) {
+                return true;
+            }
+        }
+        return false;
+	}
 
-    // computing turnaround time
-    boost::xtime endTime, turnaround;
-    boost::xtime_get(&endTime, boost::TIME_UTC);  // record the end time
-    turnaround.sec = endTime.sec - startTime.sec;
-    turnaround.nsec = endTime.nsec - startTime.nsec;
-    gc->turnaround_times.push_back(turnaround);
-    boost::mutex::scoped_lock io_lock(io_mutex);
-    double tat = (double) endTime.sec - startTime.sec +
-      ((double)endTime.nsec - startTime.nsec) / 1000000000;
-    cout << "----- Player #" << myId << " (" << myName
-       << ") finished.  Turnaround time = "
-       << tat << " seconds." << endl;
-  }
+	bool int_find(vector<int*> vect, int* item) {
+        for ( int i=0; i < vect->size(); i++) {
+            if ( *(vect[i]) == *item ) {
+                return i;
+            }
+        }
+        return -1;
+	}
 
-  // This is the function where you will spend the most effort. I left a few
-  // fragments from my solution, but it will need lots of modification.
-  void playHole(int hole) {
-    if (DEBUG) {
-      boost::mutex::scoped_lock io_lock(io_mutex);
-      cout << "##### Player #" << myId << " (" << myName
-         << ") waiting for hole " << hole << endl;
-    }
+	// This operator and the included function get called when the thread is
+	// created
+	void operator()() {
+		playCourse();
+	}
 
-    // Syntax is a little funny because we have a pointer to a generator.
-    // Normally, you just call generator()
-    double sleep_duration = (*log_norm_generator)();
+	// I don't think you will need to modfiy this function.
+	void playCourse() {
+		// Playing the course simply means playing each hole.
+		for (int i = 0; i < 18; i++) {
+			playHole(i);
+		}
 
-    if (DEBUG) {
-      boost::mutex::scoped_lock io_lock(io_mutex);
-      cout << "Player #" << myId << " (" << myName
-         << ") playing hole " << hole << " will take "
-         << sleep_duration << " seconds." << endl;
-    }
+		// computing turnaround time
+		boost::xtime endTime, turnaround;
+		boost::xtime_get(&endTime, boost::TIME_UTC);	// record the end time
+		turnaround.sec = endTime.sec - startTime.sec;
+		turnaround.nsec = endTime.nsec - startTime.nsec;
+		gc->turnaround_times.push_back(turnaround);
+		boost::mutex::scoped_lock io_lock(io_mutex);
+		double tat = (double) endTime.sec - startTime.sec +
+			((double)endTime.nsec - startTime.nsec) / 1000000000;
+		cout << "----- Player #" << myId << " (" << myName
+			 << ") finished.	Turnaround time = "
+			 << tat << " seconds." << endl;
+	}
 
-    make_thread_sleep(sleep_duration);
+	// This is the function where you will spend the most effort. I left a few
+	// fragments from my solution, but it will need lots of modification.
+	void playHole(int hole) {
+		if (DEBUG) {
+			boost::mutex::scoped_lock io_lock(io_mutex);
+			cout << "##### Player #" << myId << " (" << myName
+				 << ") waiting for hole " << hole << endl;
+		}
+        // wait for your party to catch-up
+        gc->party_barriers[int_find(gc->party_barriers, &myParty)]->wait();
 
-    if (DEBUG) {
-      boost::mutex::scoped_lock io_lock(io_mutex);
-      cout << "$$$$$ Player #" << myId << " (" << myName
-         << ") notified."  << endl;
-    }
-  }
+        gc->hole_barriers[hole]->wait();
+        boost::shared_lock lock(*(gc->hole_locks[hole]));
 
-  // Exactly one player in each party much call this function for each hole.
-  void announcePlaying(int hole) {
-    boost::mutex::scoped_lock io_lock(io_mutex);
-    cout << ">>>>> Party number " << myParty << " is playing hole "
-       << hole << endl;
-  }
+        while ((gc->party_playing_hole[hole]) != myParty && (gc->party_playing_hole[hole]) != -1) {
+            gc->hole_conditions[hole].wait(lock);
+        }
+		
 
-  // Exactly one player in each party much call this function for each hole.
-  void announceLeaving(int hole) {
-    boost::mutex::scoped_lock io_lock(io_mutex);
-    cout << "<<<<< Party number " << myParty << " is leaving hole "
-       << hole << endl;
-  }
+		// Syntax is a little funny because we have a pointer to a generator.
+		// Normally, you just call generator()
+		double sleep_duration = (*log_norm_generator)();
+
+		if (DEBUG) {
+			boost::mutex::scoped_lock io_lock(io_mutex);
+			cout << "Player #" << myId << " (" << myName
+				 << ") playing hole " << hole << " will take "
+				 << sleep_duration << " seconds." << endl;
+		}
+
+		make_thread_sleep(sleep_duration);
+
+		if (DEBUG) {
+			boost::mutex::scoped_lock io_lock(io_mutex);
+			cout << "$$$$$ Player #" << myId << " (" << myName
+				 << ") notified."	<< endl;
+		}
+	}
+
+	// Exactly one player in each party much call this function for each hole.
+	void announcePlaying(int hole) {
+		boost::mutex::scoped_lock io_lock(io_mutex);
+		cout << ">>>>> Party number " << myParty << " is playing hole "
+			 << hole << endl;
+	}
+
+	// Exactly one player in each party much call this function for each hole.
+	void announceLeaving(int hole) {
+		boost::mutex::scoped_lock io_lock(io_mutex);
+		cout << "<<<<< Party number " << myParty << " is leaving hole "
+			 << hole << endl;
+	}
 
 
 private:
-  int myId;
-  string myName;
-  double mean, sigma;
-  int myParty;
-  GolfCourse* gc;
+	int myId;
+	string myName;
+	double mean, sigma;
+	int myParty;
+	GolfCourse* gc;
 
-  boost::xtime startTime;
-  boost::mt19937 rng;
-  boost::lognormal_distribution<>* log_norm;
-  boost::variate_generator<boost::mt19937, boost::lognormal_distribution<> >*
-    log_norm_generator;
+	boost::xtime startTime;
+	boost::mt19937 rng;
+	boost::lognormal_distribution<>* log_norm;
+	boost::variate_generator<boost::mt19937, boost::lognormal_distribution<> >*
+		log_norm_generator;
 };
